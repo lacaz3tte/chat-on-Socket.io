2fc60a2373342855fa51798fa991ba5a1545ad9b
lacaz3tte <pahomov.oleg.199777@gmail.com>
2023-01-26 19:36:42 +0300

    Third commit

--- src/massage/Massage.tsx
+++ src/massage/Massage.tsx
@@ -7,7 +7,7 @@ const Massage = ({name,msg,date,foreign}:IMassage) => {
 
   return (
     <div className={'flex' + (foreign ? ' justify-end' : '')}>
-        <div className={'border p-3 max-w-[75%] overflow-auto rounded-3xl shadow-lg my-3 ' + (foreign? ' bg-hDarkGreen g-h' : ' bg-hWhite')}>
+        <div className={'border p-3 max-w-[60%] overflow-auto rounded-3xl shadow-lg my-3 ' + (foreign? ' bg-hDarkGreen g-h' : ' bg-hWhite')}>
             {name!=='' &&
                 <p className={'font-bold'+ (foreign?' text-hWhite' : '  text-hDarkGreen g-h')}>{name}</p>
             }
