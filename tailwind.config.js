e322e563defef1e18af5a319b8e946b7ee7ef6bc
lacaz3tte <pahomov.oleg.199777@gmail.com>
2023-06-04 14:42:08 +0300

    change colors for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,11 +15,11 @@ module.exports = {
         h4:'#575ccd',
         h5:'#F0F5FC',
         //dark theme
-        hd1:'#2b353a',
-        hd2:'#ffdf9e',
-        hd3:'#b38645',
-        hd4:'#0092b3',
-        hd5:'#21282c'
+        hd1:'#0b2031',
+        hd2:'#bfe7e7',
+        hd3:'#a6bdae',
+        hd4:'#39b0b2',
+        hd5:'#0E2B42'
       },
       fontFamily: {
         dune: ["Dune"],
